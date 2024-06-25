411657a2eb975c6ab030dc2c53cc4dc7075fc1dd
Wuuups <sam40907@gmail.com>
2024-06-26 01:42:32 +0800

    homepage done(maybe

--- component/home/Banner/index.jsx
+++ component/home/Banner/index.jsx
@@ -1,18 +1,48 @@
+'use client'
+import { useEffect, useRef } from 'react'
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
 import React from 'react'
 import styles from './banner.module.scss'
 
+gsap.registerPlugin(ScrollTrigger)
+
 export default function Banner() {
+  const imgRef = useRef(null)
+  const containerRef = useRef(null)
+
+  useEffect(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: containerRef.current,
+        start: 0,
+        end: '+=1000px',
+        scrub: 1,
+        pin: true,
+        anticipatePin: 1,
+      },
+    })
+
+    tl.fromTo(imgRef.current, { x: 0 }, { x: window.innerWidth * 1.5 })
+  }, [])
+
   return (
     <section>
-      <div className={styles.banner}>
-        <span>FALLING?</span>
-        <br />
-        <span>JUST PART OF</span>
-        <br />
-        <span>THE FUN.</span>
-      </div>
-      <div className={styles.skateImg}>
-        <img className={styles.objFit} src="/images/skateboard.png" />
+      <div className={styles.bannerContainer} ref={containerRef}>
+        <div className={styles.slogon}>
+          FALLING?
+          <br />
+          JUST PART OF
+          <br />
+          THE FUN.
+        </div>
+        <div className={styles.skateImg}>
+          <img
+            className={styles.objFit}
+            ref={imgRef}
+            src="/images/skateboard.png"
+          />
+        </div>
       </div>
     </section>
   )
--- component/home/Spirit/index.jsx
+++ component/home/Spirit/index.jsx
@@ -7,27 +7,56 @@ import styles from './spirit.module.scss'
 gsap.registerPlugin(ScrollTrigger)
 
 export default function Spirit() {
-  useEffect(() => {}, [])
+  const imagesRef = useRef([])
+  const spiritRef = useRef(null)
+  const textRef = useRef(null)
+  useEffect(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: spiritRef.current,
+        start: 'top top',
+        end: '+=950px',
+        scrub: 1,
+        pin: true,
+        anticipatePin: 1,
+      },
+    })
+
+    imagesRef.current.forEach((img, index) => {
+      tl.fromTo(img, { opacity: 0 }, { opacity: 1 }, index * 0.5)
+    })
+    tl.fromTo(
+      textRef.current,
+      { opacity: 0 },
+      { opacity: 1 },
+      imagesRef.current.length * 0.5
+    )
+
+    return () => {
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill())
+    }
+  }, [])
+
   return (
     <section>
-      <div className={styles.spiritContainer}>
+      <div className={styles.spiritContainer} ref={spiritRef}>
         <div className={styles.imgContainer}>
-          <img src="/images/frame1.png" />
-          <img src="/images/frame2.png" />
-          <img src="/images/frame3.png" />
-          <img src="/images/frame4.png" />
-          <img src="/images/frame5.png" />
-          <img src="/images/frame6.png" />
-          <img src="/images/frame7.png" />
+          {[...Array(9)].map((_, index) => (
+            <img
+              key={index}
+              ref={(el) => (imagesRef.current[index] = el)}
+              src={`/images/frame${index + 1}.png`}
+            />
+          ))}{' '}
+          <span className={styles.sripit} ref={textRef}>
+            At SKATT, we believe in pushing boundaries and embracing the thrill
+            of the ride. Our boards are more than just a means of
+            transportation; they are a symbol of creativity, resilience, and the
+            unyielding pursuit of excellence. Join us, and become a part of a
+            community that values individuality, innovation, and the endless
+            possibilities of the open streets.
+          </span>
         </div>
-        <p>
-          At SKATT, we believe in pushing boundaries and embracing the thrill of
-          the ride. Our boards are more than just a means of transportation;
-          they are a symbol of creativity, resilience, and the unyielding
-          pursuit of excellence. Join us, and become a part of a community that
-          values individuality, innovation, and the endless possibilities of the
-          open streets.
-        </p>
       </div>
     </section>
   )
